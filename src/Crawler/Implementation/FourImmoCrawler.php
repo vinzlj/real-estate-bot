dcb1f6cb32c5447a69477b90c6bb936e382573df
Vincent Le Jeune <vinzlj@gmail.com>
2020-05-30 19:23:36 +0200

    Add 4Immo.fr crawler

---
+++ src/Crawler/Implementation/FourImmoCrawler.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crawler\Implementation;
+
+use Crawler\AdCrawlerInterface;
+use Crawler\BaseCrawler;
+use Symfony\Component\DomCrawler\Crawler;
+
+class FourImmoCrawler extends BaseCrawler implements AdCrawlerInterface
+{
+    public function extractAdId(Crawler $adCrawler): int
+    {
+        preg_match('/article class=\".*node-(\d{1,}).*"/', $adCrawler->outerHtml(), $matches);
+
+        return (int) $matches[1];
+    }
+
+    public function extractAdUrl(Crawler $adCrawler): string
+    {
+        return sprintf('%s%s', $this->baseUrl, $adCrawler->filter('a.teaser_link')->attr('href'));
+    }
+}
